11ba5818346e34eaf9b9c62137ac26cd8e3a11a6
mizuho_shirai <nnshiro@gmail.com>
2025-09-12 22:01:11 +0900

    refactor: composition apiで書き直した

--- app/app.vue
+++ app/app.vue
@@ -1,46 +1,30 @@
 <template>
-    <!-- The AG Grid component -->
-    <ag-grid-vue
-        :rowData="rowData"
-        :columnDefs="colDefs"
-        style="height: 500px"
-    >
-    </ag-grid-vue>
-
+  <h1>Hello AG-Grid</h1>
+  <ag-grid-vue :rowData="rowData" :columnDefs="colDefs" style="height: 500px">
+  </ag-grid-vue>
 </template>
 
-<script>
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
+<script setup>
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AgGridVue } from "ag-grid-vue3";
 
-// Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
-import { AgGridVue } from "ag-grid-vue3"; // Vue Data Grid Component
 
-export default {
-    name: "App",
-    components: {
-        AgGridVue, // Add Vue Data Grid component
-    },
-    setup() {
-    // Row Data: The data to be displayed.
-    const rowData = ref([
-        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-        { make: "Ford", model: "F-Series", price: 33850, electric: false },
-        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    ]);
+// Row Data: The data to be displayed.
+const rowData = ref([
+  { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  { make: "Ford", model: "F-Series", price: 33850, electric: false },
+  { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+]);
 
-    // Column Definitions: Defines the columns to be displayed.
-    const colDefs = ref([
-        { field: "make" },
-        { field: "model" },
-        { field: "price" },
-        { field: "electric" }
-    ]);
-
-    return {
-        rowData,
-        colDefs,
-    };
-}
-};
+// Column Definitions: Defines the columns to be displayed.
+const colDefs = ref([
+  { field: "make" },
+  { field: "model" },
+  { field: "price" },
+  { field: "electric" },
+]);
 </script>
+
+<style scoped>
+</style>
