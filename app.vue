9390d85437037ec6661caf82c06c210b364d8728
mizuho_shirai <nnshiro@gmail.com>
2025-09-13 18:06:01 +0900

    [feat] add /pages and index.vue

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  RowDragModule,
+} from "ag-grid-community";
+import { AgGridVue } from "ag-grid-vue3";
+
+ModuleRegistry.registerModules([AllCommunityModule, RowDragModule]);
+// ModuleRegistry.registerModules([
+//   TextFilterModule,
+//   NumberFilterModule,
+//   RowDragModule,
+//   ClientSideRowModelModule,
+// ]);
+
+const rowData = ref([
+  { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  { make: "Ford", model: "F-Series", price: 33850, electric: false },
+  { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+]);
+
+const colDefs = ref([
+  { field: "make", editable: true, rowDrag: true, rowDragManaged: true },
+  { field: "model", editable: false },
+  { field: "price", editable: false },
+  { field: "electric", editable: false },
+]);
+</script>
+
+<style scoped></style>
